90f18a07e5d3ca65fe249af4136c34b74cc15545
GuilhermeS94 <GuilhermeS94@users.noreply.github.com>
2021-04-02 18:28:08 -0300

    feat: build apps android e ios, customizando css, customizando icones do app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Plugins } from "@capacitor/core";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,7 +22,10 @@ import '@ionic/react/css/display.css';
 
 import "./index.css";
 
+const { SplashScreen } = Plugins;
+
 ReactDOM.render(<App />, document.getElementById('root'));
+SplashScreen.hide();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
